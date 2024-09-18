d1ccaac3d1ce385249ef4238a353a78548f15f42
shatnawi86 <mshatnawi@uri.edu>
2024-09-18 12:45:02 +0300

    Add project files.

---
+++ JordanChat/JordanChat/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace JordanChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public ChatHub() { }
+
+        public async Task JoinAsync(string channel)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, channel);
+        }
+
+        public async Task BroadcastMessageAsync(string message, string channel)
+        {
+            await Clients.Group(channel).SendAsync("OnBroadcast", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine(Context.ConnectionId + " is connected!");
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine(Context.ConnectionId + " has disconnected!");
+
+        }
+    }
+}
